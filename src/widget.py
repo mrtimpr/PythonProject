72624e021c87aa4f56b1d7136745767f041909bd
Timofey <mr.tim.iv@gmail.com>
2025-10-24 19:19:42 +0300

    update widget.py (add new function: get_date)

--- src/widget.py
+++ src/widget.py
@@ -10,3 +10,10 @@ def mask_account_card(users_data: str) -> str:
     else:
         bank_card_number = users_data[-16:]
         return users_data[:-16] + masks.get_mask_card_number(bank_card_number)
+
+
+def get_date(date_and_time: str) -> str:
+    """функция, которая принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" """
+    received_date = date_and_time[8:10] + "." + date_and_time[5:7] + "." + date_and_time[:4]
+    return received_date
